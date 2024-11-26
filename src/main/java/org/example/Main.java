a312c55c37cea2f3c3fc57dce27dbe106227a660
mazaeva-m-a <m30061494@gmail.com>
2024-11-26 11:44:10 +0700

    Практическая 4
    Signed-off-by: mazaeva-m-a <m30061494@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-     printColor();
+     compareNumbers();
     }
 
         //1
@@ -16,15 +16,16 @@ public class Main {
         }
 
         //2
-        public static void checkSumSign () {
-        int a = 10;
-        int b = 18;
-        if (a+b >= 0) {
+        public  void checkSumSign () {
+            boolean result;
+            int a = 5;
+            int b =-25;
+            if (a+b >= 0) {
                 System.out.println("Сумма положительная");
             }
-        else
-            System.out.println("Сумма отрицательная");
+            else System.out.println("Сумма отрицательная");
         }
+
 
         //3
         public static void printColor () {
